e78964910ac0b3065bfa16079819db59b56c20d0
Guigo-737 <rodrigohernandes@colegiopaulodetarso.com.br>
2026-03-24 14:34:02 +0000

    a

--- script.js
+++ script.js
@@ -3,5 +3,21 @@ const taskList = document.querySelector("#taskList")
 const taskInput = document.querySelector("#taskInput")
 
 taskBtn.addEventListener("click", function(){
-    alert("CLIQUEI !!!")
+    const li = document.createElement('li');
+    const taskInputValue = taskInput.value;
+    if(taskInpunVelue == ''){
+        return
+    }
+    const span = document.createElement('span';)
+    span.textContent = taskInputValue;
+    const button = documenct.creatElement('button');
+    button.textContent = 'Excluir'
+    button.addEventListener('click',function(){
+    li.appendChild(span)
+    li.appendChild(button)
+    })
+
+    if (taskInput)
+    li.textContent = taskInputValue;
+    taskList.appendChild(li);
 })
