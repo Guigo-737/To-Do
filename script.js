2e30555890051bbb1ea84f0f5339138a320cee8a
Guigo-737 <rodrigohernandes@colegiopaulodetarso.com.br>
2026-03-31 14:29:30 +0000

    aaai aaaai aaaaaaaaaaai aaaaaaaaaaaaaaai

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ const taskBtn = document.querySelector("#taskBtn")
 const taskList = document.querySelector("#taskList")
 const taskInput = document.querySelector("#taskInput")
 
-taskBtn.addEventListener("click", function(){
+function createSpan(taskInputValue){
+    const span = document.createElement('span');
+    span.textContent = taskInputValue;
+    return span;
+}
+
+function createDeleteButton(taskInputValue){
+    const button = document.createElement('button');
+    button.textContent = 'Excluir';
+    button.addEventListener('click', function(){
+        li.remove();
+})
+
+function addTask(){
     const li = document.createElement('li');
-    const taskInputValue = taskInput.value;
-    if(taskInpunVelue == ''){
+    const taskInputValue = taskInput.value; 
+    if (taskInputValue == '')
         return
     }
-    const span = document.createElement('span';)
-    span.textContent = taskInputValue;
-    const button = documenct.creatElement('button');
-    button.textContent = 'Excluir'
-    button.addEventListener('click',function(){
-    li.appendChild(span)
-    li.appendChild(button)
-    })
+    const span = createSpan(taskInputValue);
+    const deleteBtn = createDeleteButton(li)
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
 
-    if (taskInput)
-    li.textContent = taskInputValue;
     taskList.appendChild(li);
-})
+
+}
+taskBtn.addEventListener("click", addTask)                                                                  
